da9e3cb1172c11e3d142af97c8290001899f5be8
Marcel Erz <me@marcelerz.com>
2015-03-08 01:39:05 -0800

    Initial commit

---
+++ test/fixtures/expected/test.js
@@ -0,0 +1,9 @@
+
+var __cov_ = (Function('return this'))();
+if (!__cov_.__coverage__) { __cov_.__coverage__ = {}; }
+__cov_ = __cov_.__coverage__;
+if (!(__cov_['source/test.js'])) {
+   __cov_['source/test.js'] = {"path":"source/test.js","s":{"1":0,"2":0},"b":{},"f":{},"fnMap":{},"statementMap":{"1":{"start":{"line":1,"column":0},"end":{"line":1,"column":14}},"2":{"start":{"line":3,"column":0},"end":{"line":3,"column":18}}},"branchMap":{}};
+}
+__cov_ = __cov_['source/test.js'];
+__cov_.s['1']++;var test=23;__cov_.s['2']++;console.log(test);
---
+++ test/fixtures/source/test.js
@@ -0,0 +1,3 @@
+var test = 23;
+
+console.log(test);
